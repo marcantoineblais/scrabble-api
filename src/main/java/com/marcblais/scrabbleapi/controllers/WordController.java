f56a9cee3f2c24d492147d22f62175195a400128
Marc Blais <marcantblais@gmail.com>
2024-01-12 15:40:41 -0500

    tried to make program faster

--- src/main/java/com/marcblais/scrabbleapi/controllers/WordController.java
+++ src/main/java/com/marcblais/scrabbleapi/controllers/WordController.java
@@ -4,6 +4,7 @@ import com.marcblais.scrabbleapi.dto.*;
 import com.marcblais.scrabbleapi.entities.Language;
 import com.marcblais.scrabbleapi.entities.DictionaryEntry;
 import com.marcblais.scrabbleapi.services.WordService;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,9 +38,9 @@ public class WordController {
     @CrossOrigin(origins = "http://localhost:3000")
     public List<Solution> findWordsThatFitsOnGrid(@RequestBody Grid grid) {
         long startTime = System.currentTimeMillis();
+
         List<DictionaryEntry> entries = wordService.findWordsByLanguage(grid.getLanguage());
         List<GridContent> gridContents = grid.toGridContent();
-
         SolutionsFinder solutionsFinder = new SolutionsFinder(grid, entries, gridContents);
         List<Solution> solutions = solutionsFinder.toSolutions();
 
